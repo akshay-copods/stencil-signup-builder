c267326cb1af3071c6c9a1b47f43f9f9ea1761c9
Akshay Kamble <akshay.kamble@copods.co>
2023-03-16 17:35:07 +0530

    update tailwind colors

--- packages/stencil-library/tailwind.config.js
+++ packages/stencil-library/tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx,html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        geekblue: {
+          100: '#f0f5ff',
+          200: '#d6e4ff',
+          300: '#adc6ff',
+          400: '#85a5ff',
+          500: '#597ef7',
+          600: '#2f54eb',
+          700: '#1d39c4',
+          800: '#10239e',
+          900: '#061178',
+          1000: '#061178',
+        },
+        sunsetOrange: {
+          200: '#ffe7ba',
+        },
+        customBlack: {
+          400: '#00000073',
+          600: '#000000D9',
+        },
+      },
+      fontSize: {
+        h1: '2.375rem',
+        h2: '1.875rem',
+        h3: '1.5rem',
+        h4: '1.25rem',
+      },
+    },
   },
   plugins: [],
-}
+};
